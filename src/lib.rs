6f6c3a2c2d04e8ba12c32ee97bef148f01094616
k1nd0ne <k1nd0ne@mail.com>
2024-12-22 17:47:42 +0100

    Transforming in lib

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+pub mod extfs;
+pub mod image;
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-mod extfs;
-mod image;
-
-use image::Image;
-fn main() {
-    println!("Hello, world!");
-    let _img: Image = Image::new("test".to_string());
-}
