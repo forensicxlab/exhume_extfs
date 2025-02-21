7071e673130a010c3e73e8574470109c1fb9e597
k1nd0ne <k1nd0ne@mail.com>
2025-02-21 19:24:26 +0100

    Adding loggin

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod extfs;
 
-use clap::{Arg, ArgAction, Command};
+use clap::{value_parser, Arg, ArgAction, Command};
 use clap_num::maybe_hex;
 use exhume_body::Body;
 use extfs::ExtFS;
-use std::io::Read;
+use log::{debug, error, info};
 
 fn process_partition(
     file_path: &str,
@@ -14,55 +14,53 @@ fn process_partition(
     inode_number: &usize,
     groupdesc: &bool,
     json: &bool,
-    verbose: &bool,
 ) {
     let mut body = Body::new_from(file_path.to_string(), format, Some(*offset));
-    if *verbose {
-        body.print_info();
-    }
-
-    // let lvm = Lvm2::open(&mut body, *offset).unwrap();
-    // let lv = lvm.lvs().next().unwrap();
-    // println!("LV {}", lv.name());
-    // println!("{:?}", lv.raw_metadata());
-    // let mut olv = lvm.open_lv(lv, &mut body);
-
-    // let mut buf = [0u8; 1024];
-    // olv.read_exact(&mut buf).unwrap();
-    // for _ in 0..16 {
-    //     let mut buf2 = [0u8; 16];
-    //     olv.read_exact(&mut buf2).unwrap();
-    //     println!("{buf2:x?}");
-    // }
+    // Log body creation at debug level.
+    debug!("Created Body from '{}'", file_path);
 
     let mut filesystem = match ExtFS::new(&mut body, *offset) {
         Ok(fs) => Some(fs),
         Err(message) => {
-            eprintln!("ExtFS object creation error: {}", message);
+            error!("ExtFS object creation error: {}", message);
             None
         }
     };
 
     if let Some(fs) = &mut filesystem {
-        if *verbose {
-            println!("ExtFS created successfully.");
-        }
+        info!("ExtFS created successfully.");
         if *superblock {
             fs.superblock.print_sp_info();
         }
 
         if *inode_number > 0 {
-            match fs.load_group_descriptors() {
-                Ok(_) => {}
-                Err(err) => eprintln!("{}", err),
-            };
+            if let Err(err) = fs.load_group_descriptors() {
+                error!("{}", err);
+            }
             let inode = match fs.get_inode(*inode_number as u32) {
                 Ok(inode) => inode,
                 Err(err) => {
-                    eprintln!("{}", err);
+                    error!("{}", err);
                     std::process::exit(1);
                 }
             };
+
+            // let file = match fs.read_file("/abi-3.13.0-24-generic") {
+            //     Ok(file) => file,
+            //     Err(err) => {
+            //         error!("{}", err);
+            //         std::process::exit(1);
+            //     }
+            // };
+            // info!("File read: {:?}", file);
+            if *json {
+                match serde_json::to_string_pretty(&inode.to_json()) {
+                    Ok(json_str) => info!("{}", json_str),
+                    Err(e) => error!("Error serializing inode: {}", e),
+                }
+            } else {
+                info!("Display the prettytable here");
+            }
 
             // let test = match fs.read(*inode_number as u32) {
             //     Ok(test) => test,
@@ -72,44 +70,24 @@ fn process_partition(
             //     }
             // };
             // println!("{:?}", String::from_utf8_lossy(&test));
-
-            let file = match fs.read_file("/abi-3.13.0-24-generic") {
-                Ok(file) => file,
-                Err(err) => {
-                    eprintln!("{}", err);
-                    std::process::exit(1);
-                }
-            };
-            println!("{:?}", file);
-            if *json {
-                println!(
-                    "{}",
-                    serde_json::to_string_pretty(&inode.to_json()).unwrap()
-                );
-            } else {
-                println!("Display the prettytable here");
-            }
         }
 
         if *groupdesc {
-            match fs.load_group_descriptors() {
-                Ok(_) => {
-                    if *json {
-                        let json_array: Vec<_> = fs
-                            .get_bg_descriptors()
-                            .unwrap()
-                            .iter()
-                            .map(|group_descriptor| group_descriptor.to_json())
-                            .collect();
-
-                        println!("{}", serde_json::to_string_pretty(&json_array).unwrap());
-                    } else {
-                        println!("Display the prettytable here");
+            if let Err(err) = fs.load_group_descriptors() {
+                error!("{}", err);
+                std::process::exit(1);
+            } else {
+                if *json {
+                    if let Ok(bg_descriptors) = fs.get_bg_descriptors() {
+                        let json_array: Vec<_> =
+                            bg_descriptors.iter().map(|gd| gd.to_json()).collect();
+                        match serde_json::to_string_pretty(&json_array) {
+                            Ok(json_str) => info!("{}", json_str),
+                            Err(e) => error!("Error serializing group descriptors: {}", e),
+                        }
                     }
-                }
-                Err(err) => {
-                    eprintln!("{}", err);
-                    std::process::exit(1);
+                } else {
+                    info!("Display the prettytable here");
                 }
             }
         }
@@ -125,7 +103,7 @@ fn main() {
             Arg::new("body")
                 .short('b')
                 .long("body")
-                .value_parser(clap::value_parser!(String))
+                .value_parser(value_parser!(String))
                 .required(true)
                 .help("The path to the body to exhume."),
         )
@@ -133,7 +111,7 @@ fn main() {
             Arg::new("format")
                 .short('f')
                 .long("format")
-                .value_parser(clap::value_parser!(String))
+                .value_parser(value_parser!(String))
                 .required(true)
                 .help("The format of the file, either 'raw' or 'ewf'."),
         )
@@ -168,44 +146,42 @@ fn main() {
                 .help("Display the group descriptors"),
         )
         .arg(
-            Arg::new("verbose")
-                .short('v')
-                .long("verbose")
-                .action(ArgAction::SetTrue),
-        )
-        .arg(
             Arg::new("json")
                 .short('j')
                 .long("json")
                 .action(ArgAction::SetTrue),
         )
+        .arg(
+            Arg::new("log_level")
+                .short('l')
+                .long("log-level")
+                .value_parser(["error", "warn", "info", "debug", "trace"])
+                .default_value("info")
+                .help("Set the log verbosity level"),
+        )
         .get_matches();
+
+    // Initialize logger.
+    let log_level_str = matches.get_one::<String>("log_level").unwrap();
+    let level_filter = match log_level_str.as_str() {
+        "error" => log::LevelFilter::Error,
+        "warn" => log::LevelFilter::Warn,
+        "info" => log::LevelFilter::Info,
+        "debug" => log::LevelFilter::Debug,
+        "trace" => log::LevelFilter::Trace,
+        _ => log::LevelFilter::Info,
+    };
+    env_logger::Builder::new().filter_level(level_filter).init();
 
     let file_path = matches.get_one::<String>("body").unwrap();
     let format = matches.get_one::<String>("format").unwrap();
     let offset = matches.get_one::<u64>("offset").unwrap();
-    let verbose = match matches.get_one::<bool>("verbose") {
-        Some(verbose) => verbose,
-        None => &false,
-    };
-    let superblock = match matches.get_one::<bool>("superblock") {
-        Some(superblock) => superblock,
-        None => &false,
-    };
-    let groupdesc = match matches.get_one::<bool>("groupdesc") {
-        Some(groupdesc) => groupdesc,
-        None => &false,
-    };
-    let inode = match matches.get_one::<usize>("inode") {
-        Some(inode) => inode,
-        None => &0usize,
-    };
-    let json = match matches.get_one::<bool>("json") {
-        Some(json) => json,
-        None => &false,
-    };
+    let superblock = matches.get_one::<bool>("superblock").unwrap_or(&false);
+    let groupdesc = matches.get_one::<bool>("groupdesc").unwrap_or(&false);
+    let inode = matches.get_one::<usize>("inode").unwrap_or(&0usize);
+    let json = matches.get_one::<bool>("json").unwrap_or(&false);
 
     process_partition(
-        file_path, format, offset, superblock, inode, groupdesc, json, verbose,
+        file_path, format, offset, superblock, inode, groupdesc, json,
     );
 }
